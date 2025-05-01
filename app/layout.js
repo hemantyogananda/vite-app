066156536d3e8b6084506de8202a4dcd1b2034e5
hemantyogananda <hemant123yoga@gmail.com>
2025-05-01 20:12:58 +0530

    Added app folder

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+    title: 'My App',
+    description: 'My App is a...',
+  }
+   
+  export default function RootLayout() {
+    return (
+      <html lang="en">
+        <body>
+          <div id="root">hello</div>
+        </body>
+      </html>
+    )
+  }
